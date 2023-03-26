36615fd5c5c69724dd4a79692c23d7d8f73429b4
KMeniawy <karim.elmeniawy.99@gmail.com>
2023-03-26 11:23:42 +0200

    loader + newsfeed end

--- React Blog/src/components/PostCard.jsx
+++ React Blog/src/components/PostCard.jsx
@@ -5,9 +5,10 @@ export default function PostCard() {
         <img src="./src/assets/Imgs/pic (1).jpg"/>
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Dummy data</h2>
+        <h2 className="card-title">My Cat Is Watching me</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, culpa?</p>
         <div className="card-actions justify-end">
+          <p>hello this is the author</p>
           <button className="btn btn-primary">Read More</button>
         </div>
       </div>
--- React Blog/src/pages/Home.jsx
+++ React Blog/src/pages/Home.jsx
@@ -1,10 +1,44 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { render } from "react-dom";
+
 import PostCard from "../components/PostCard";
+import CheckNews from "../components/Icons/CheckNews";
+import Loader from "../components/shared/Loader";
 
 export default function Home() {
-    return(
-        <div className="m-20">
-            <PostCard/>
-            <PostCard/>
+//--------------states-----------------
+  const [postData, setPostData] = useState([]);
+//--------------effect-----------------
+  useEffect(async () => {
+    async function getPosts(){
+        const { data } = await axios.get('http://localhost:3001/v1/post');
+        setPostData(data);
+    }
+
+    getPosts();
+  }, []);
+
+//   const showPosts = postData.map((postData, index) => {
+//     return;
+//   });
+
+  return (
+    <div className="m-20 p-5">
+      {postData.length === 0 && (
+        <div className="flex justify-center mb-10">
+          <Loader />
         </div>
-    )
+      )}
+
+      <PostCard />
+      <br />
+      <div className="flex justify-center mt-10"> {/* NOTE : this is the end of the news feed */}
+        <hr className="border-amber-600 border-2 relative w-20 my-10 mr-2" />
+        <h2 className="text-3xl pt-4 py-2">you're all caught up</h2>{" "}
+        <CheckNews className="p-1" />
+        <hr className="border-amber-600 border-2 relative w-20 my-10 ml-2" />
+      </div>
+    </div>
+  );
 }
