84de387d723fa4191dcf31e57d0c6c63321dc59c
KMeniawy <karim.elmeniawy.99@gmail.com>
2023-04-07 11:35:26 +0200

    home responsive

--- React Blog/src/components/PostCard.jsx
+++ React Blog/src/components/PostCard.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 const count = 50;
 export default function PostCard(props) {
   return (
-    <div className="card lg:card-side bg-indigo-900 shadow-md m-10 h-64 hover:bg-secondary hover:shadow-primary hover:transition-all">
-      <figure className="w-96">
-        <img src={props.photo[0].url} />
+    <div className="card lg:card-side bg-indigo-900 shadow-md m-10 hover:bg-secondary hover:shadow-primary hover:transition-all sm:h-1/5">
+      <figure className="sm:w-auto lg:w-[400px] bg-white lg:bg-none lg:ml-o">
+        <img src={props.photo[0].url} className="w-auto xl:overflow-hidden lg:bg-center"/>
       </figure>
       <div className="card-body">
         <h2 className="card-title font-body">{props.title}</h2>
